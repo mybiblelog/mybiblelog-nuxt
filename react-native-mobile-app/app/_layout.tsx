92b4309f97be23f70eb32354f20d53596417e60b
Aaron Smith <35936976+AaronSmithX@users.noreply.github.com>
2026-01-30 22:03:45 -0500

    Success toast for app settings updates

--- react-native-mobile-app/app/_layout.tsx
+++ react-native-mobile-app/app/_layout.tsx
@@ -4,18 +4,21 @@ import { ThemeProvider, useTheme } from "@/src/theme/ThemeProvider";
 import { AuthProvider } from "@/src/auth/AuthProvider";
 import { LogEntriesProvider } from "@/src/log-entries/LogEntriesProvider";
 import { UserSettingsProvider } from "@/src/settings/UserSettingsProvider";
+import { ToastProvider } from "@/src/toast/ToastProvider";
 
 export default function RootLayout() {
   return (
     <LocaleProvider>
       <ThemeProvider>
-        <AuthProvider>
-          <UserSettingsProvider>
-            <LogEntriesProvider>
-              <RootStack />
-            </LogEntriesProvider>
-          </UserSettingsProvider>
-        </AuthProvider>
+        <ToastProvider>
+          <AuthProvider>
+            <UserSettingsProvider>
+              <LogEntriesProvider>
+                <RootStack />
+              </LogEntriesProvider>
+            </UserSettingsProvider>
+          </AuthProvider>
+        </ToastProvider>
       </ThemeProvider>
     </LocaleProvider>
   );
