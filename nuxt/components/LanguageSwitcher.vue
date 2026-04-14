5eb3b4958690254c5fd0a9bf75e81d04f160c198
Aaron Smith <35936976+AaronSmithX@users.noreply.github.com>
2026-04-14 07:09:47 -0400

    Improve language switcher

--- nuxt/components/LanguageSwitcher.vue
+++ nuxt/components/LanguageSwitcher.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="navbar-item has-dropdown is-hoverable">
-    <div class="navbar-link desktop-switcher">
+  <div class="navbar-item language-switcher">
+    <a
+      href="#"
+      class="language-switcher-desktop desktop-switcher"
+      role="button"
+      :aria-label="$t('choose_language')"
+      @click.prevent="modalOpen = true"
+    >
       <div class="line-height-stabilizer">
         <translator-icon />
         <span class="is-hidden-desktop"> {{ $t('choose_language') }}</span>
       </div>
-    </div>
-    <div class="navbar-dropdown is-right desktop-switcher">
-      <a v-for="locale in availableLocales" :key="locale.code" class="navbar-item" href="#" @click.prevent.stop="setLocale(locale.code)">
-        <strong v-if="locale.code === $i18n.locale">{{ locale.name }}</strong>
-        <span v-else>{{ locale.name }}</span>
-      </a>
-    </div>
+    </a>
     <div class="buttons is-centered mobile-switcher">
       <button class="button is-outline" @click="modalOpen = true">
         <translator-icon width="20" height="20" />
@@ -20,11 +20,19 @@
     </div>
     <app-modal :open="modalOpen" :title="'🌎 ' + $t('choose_language')" @close="modalOpen = false">
       <template slot="content">
-        <div class="language-buttons is-flex is-flex-direction-column is-align-items-stretch is-flex-gap-1">
-          <a v-for="locale in availableLocales" :key="locale.code" class="button" href="#" @click.prevent.stop="() => { modalOpen = false; setLocale(locale.code); }">
-            <strong v-if="locale.code === $i18n.locale">{{ locale.name }}</strong>
-            <span v-else>{{ locale.name }}</span>
-          </a>
+        <div class="language-switcher-modal">
+          <div class="language-switcher-modal__grid">
+            <a
+              v-for="locale in availableLocales"
+              :key="locale.code"
+              class="button language-switcher-modal__btn"
+              href="#"
+              @click.prevent.stop="() => { modalOpen = false; setLocale(locale.code); }"
+            >
+              <strong v-if="locale.code === $i18n.locale">{{ locale.name }}</strong>
+              <span v-else>{{ locale.name }}</span>
+            </a>
+          </div>
         </div>
       </template>
     </app-modal>
@@ -77,6 +85,32 @@ export default {
   gap: 0.68rem;
 }
 
+.language-switcher-desktop {
+  display: inline-flex;
+  align-items: center;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 0.7);
+  text-decoration: none;
+  cursor: pointer;
+  margin: -0.5rem -0.75rem;
+  padding: 1rem 1.5rem;
+}
+
+.language-switcher-desktop:hover,
+.language-switcher-desktop:focus {
+  background-color: #e8e8e8;
+  color: rgba(0, 0, 0, 0.7);
+}
+
+.language-switcher-desktop:focus:not(:focus-visible) {
+  outline: none;
+}
+
+.language-switcher-desktop:focus-visible {
+  outline: 2px solid #485fc7;
+  outline-offset: 2px;
+}
+
 .mobile-switcher {
   display: none;
   padding: 1rem;
@@ -91,8 +125,29 @@ export default {
   }
 }
 
-.language-buttons {
+.language-switcher-modal {
+  /* stylelint-disable-next-line property-no-unknown */
+  container-type: inline-size;
+  max-width: 100%;
+}
+
+.language-switcher-modal__grid {
+  display: grid;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
   gap: 0.5rem;
+  max-width: 100%;
+}
+
+/* stylelint-disable-next-line at-rule-no-unknown */
+@container (max-width: 300px) {
+  .language-switcher-modal__grid {
+    grid-template-columns: minmax(0, 1fr);
+  }
+}
+
+.language-switcher-modal__btn {
+  width: 100%;
+  justify-content: center;
 }
 </style>
 
